f6b8ae490d40387f276b13e9c5c0008118633dc4
Gabriel Arce <gab.arpe@gmail.com>
2018-09-19 18:07:04 -0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -85,6 +85,26 @@ UserSchema.pre('save',function(next){
 
 })
 
+UserSchema.statics.findByCredentials = function(email, password){
+    User = this
+    return User.findOne({email}).then((user)=>{
+        if(!user){
+            return Promise.reject()
+        }
+        return new Promise((resolve,reject)=>{
+            bcrypt.compare(password,user.password,(err,res)=>{
+                if(!res){
+                   return reject()
+                }
+                return resolve(user)
+            })
+        })
+       
+    }).catch((e)=>{
+        return Promise.reject(e)
+    })
+}
+
 var User = mongoose.model('User',UserSchema)
 
 module.exports = {
